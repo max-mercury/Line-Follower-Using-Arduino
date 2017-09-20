70ee26ee6d3abccd014a7c3ae01e0a8f623cc30f
Mayur Bharankar <31626700+max-mercury@users.noreply.github.com>
2017-09-20 23:39:30 +0530

    Add files via upload

---
+++ linefollow.ino
@@ -0,0 +1,11 @@
+#include <MotorControl.h>
+#include <LineFollower.h>
+
+MotorControl motorcntrl(1,2,3,4,7,5);
+LineFollower lineflw(A0,A1,A2,A3,A4,13);
+void setup() {
+}
+
+void loop() {
+  lineflw.line_follower();
+}
